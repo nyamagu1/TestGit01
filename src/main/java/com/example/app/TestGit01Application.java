0b92982e9ca3d06beadf149c1d2078460db0d492
eireirjp <eireirjp@gmail.com>
2026-02-05 15:02:25 +0900

    first commit

---
+++ src/main/java/com/example/app/TestGit01Application.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGit01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGit01Application.class, args);
+	}
+
+}
